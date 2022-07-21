169abfa1bbb24d03266c639480977be2feac73f6
Jaxaver <jrmajano@gmail.com>
2022-07-21 14:41:25 +0200

    third try

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -5,43 +5,43 @@ import CommentForm from './CommentForm';
 
 class Comments extends Component {
     state = {
-        commentsVisible:false,
+        commentsVisible: false,
     }
 
     hideComments = () => {
         console.log("first is", this.state.commentsVisible)
-        this.setState({commentsVisible : false})
-        console.log("then after function",this.state.commentsVisible)
+        this.setState({ commentsVisible: false })
+        console.log("then after function", this.state.commentsVisible)
     }
 
     showComments = () => {
         console.log("first is", this.state.commentsVisible)
 
-        this.setState({commentsVisible : true})
-        console.log("then after function",this.state.commentsVisible)
+        this.setState({ commentsVisible: true })
+        console.log("then after function", this.state.commentsVisible)
     }
- 
+
     render() {
         return (
             <span>
-                <button onClick={() =>this.hideComments()}>hide comments</button><button onClick={() =>this.showComments()}>show comments</button>
+                <button onClick={() => this.hideComments()}>hide comments</button><button onClick={() => this.showComments()}>show comments</button>
                 {
-                    this.state.commentsVisible &&<div className='container px-5  pb-5 bg-black border-top border-light text-white '>
-                <h3 className='pt-4 pl-5 bg-secondary text-center pb-4'><u>Comments:</u></h3>
-                <CommentForm {...this.props}/>
+                    this.state.commentsVisible && <div className='container px-5  pb-5 bg-black border-top border-light text-white '>
+                        <h3 className='pt-4 pl-5 bg-secondary text-center pb-4'><u>Comments:</u></h3>
+                        <CommentForm {...this.props} />
 
-                <ul>{this.props.video.comments.map((comment, index) =>
-                    <span key={comment[2]}>
-                        <h5>{comment[0]}:</h5>
-                        <p>{comment[1]}</p>
-                    </span>
-                )}</ul>
+                        <ul>{this.props.video.comments.map((comment, index) =>
+                            <span key={comment[2]}>
+                                <h5>{comment[0]}:</h5>
+                                <p>{comment[1]}</p>
+                            </span>
+                        )}</ul>
 
-            </div>
+                    </div>
                 }
 
             </span>
-            
+
 
 
         );
